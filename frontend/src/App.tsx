ed2c9036908ee77b003b8ee29df704e52cb81c63
Genadi Kolev <genadimkolev@gmail.com>
2024-04-22 17:16:36 +0300

    change: move react-router code into <App />

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,35 @@
-import { Outlet } from "react-router-dom";
-import "./App.css";
-import Navbar from "./components/Navbar";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "fomantic-ui-css/semantic.css";
+import pages from "./pages/index.ts";
+import PageLayout from "./layout/index.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <PageLayout />,
+    children: [
+      {
+        path: "/",
+        children: [...pages.unprotected],
+      },
+      {
+        path: "/client",
+        children: [...pages.client],
+      },
+      {
+        path: "/backoffice",
+        children: [...pages.backoffice],
+      },
+      {
+        path: "/admin",
+        children: [...pages.admin],
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,37 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import App from "./App.tsx";
 import "./index.css";
-import pages from "./pages/index.ts";
+import App from "./App";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        children: [...pages.unprotected]
-      },
-      {
-        path: "/client",
-        children: [...pages.client],
-      },
-      {
-        path: "/backoffice",
-        children: [...pages.backoffice],
-      },
-      {
-        path: "/admin",
-        children: [...pages.admin],
-      },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
