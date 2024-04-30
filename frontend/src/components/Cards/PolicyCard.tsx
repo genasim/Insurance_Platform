5477312ac2dd85bec31c5685069030d83a6ff7f4
Genadi Kolev <genadimkolev@gmail.com>
2024-05-01 00:51:40 +0300

    UI View -> landing page (PR #2)
    * add: 404 page
    
    * add: useAsyncEffect custom hook
    
    * feat: add policy card
    
    * add policy card component
    
    * update Home page with policy cards
    
    * change: move Policy card in Cards dir
    
    * feat: Why us section to Home page
    
    * fix: lint Policy card code and fix key ids

---
+++ frontend/src/components/Cards/PolicyCard.tsx
@@ -0,0 +1,61 @@
+import { FC } from "react";
+import { Button, Card, Grid, Icon, Image, Segment } from "semantic-ui-react";
+import { Policy } from "../../models/policy";
+
+export interface PolicyCardProps {
+  policy: Policy;
+  onButtonClick?: () => void;
+}
+
+const PolicyCard: FC<PolicyCardProps> = ({ policy, onButtonClick }) => {
+  return (
+    <Card raised link style={{ borderRadius: "2rem" }}>
+      <Grid>
+        <Grid.Column width={8}>
+          <Image
+            className="container segment basic very padded"
+            src={policy.imgSrc}
+            style={{ borderRadius: "2rem 0 0 2rem" }}
+          />
+        </Grid.Column>
+        <Grid.Column stretched width={8}>
+          <Segment
+            padded
+            basic
+            style={{ display: "flex", flexDirection: "column" }}
+          >
+            <Card.Header className="ui large blue header">
+              {policy.header}
+            </Card.Header>
+            <div
+              className="container segment basic very padded"
+              style={{ flexGrow: 1 }}
+            >
+              {policy.desctiption.map((descr) => (
+                <div key={descr}>
+                  <span className="ui large sub margin text">
+                    <i className="check circle outline icon"></i>
+                    {descr}
+                  </span>
+                </div>
+              ))}
+            </div>
+            <Button
+              size="medium"
+              compact
+              circular
+              fluid
+              primary
+              onClick={onButtonClick}
+            >
+              <Icon name="shopping cart" />
+              Buy policy
+            </Button>
+          </Segment>
+        </Grid.Column>
+      </Grid>
+    </Card>
+  );
+};
+
+export default PolicyCard;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,49 @@
 import React from "react";
+import {
+  CardGroup,
+  Container,
+  Divider,
+  Header,
+  Segment,
+} from "semantic-ui-react";
+import PolicyCard from "../components/Cards/PolicyCard";
+import { mock_policies } from "../models/policy";
+import HomeCard from "../components/Cards/HomeCard";
+import { homeCards } from "../models/home-card";
 
 interface HomeProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-const Home: React.FC<HomeProps> = (props) => {
-  const { ...rest } = props;
-
-  return <div {...rest}>Home page</div>;
+const Home: React.FC<HomeProps> = () => {
+  return (
+    <Container>
+      <Container fluid textAlign="center">
+        <Segment basic padded="very">
+          <Header as="span" className="ui huge text">
+            BUY AN ONLINE INSURENCE TODAY!
+          </Header>
+          <Divider hidden />
+          <Header.Subheader as="span" className="ui center massive text">
+            Reliable premium insurance experience
+          </Header.Subheader>
+        </Segment>
+      </Container>
+      <Divider section hidden />
+      <CardGroup centered stackable itemsPerRow={2}>
+        {mock_policies.map((policy) => (
+          <PolicyCard key={policy.id} policy={policy} />
+        ))}
+      </CardGroup>
+      <Divider hidden section />
+      <Divider hidden section />
+      <Header textAlign="center">
+        <span className="ui huge text">Why us</span>
+      </Header>
+      <Divider hidden />
+      <CardGroup centered stackable >
+        {homeCards.map(card => <HomeCard key={card.id} card={card} />)}
+      </CardGroup>
+    </Container>
+  );
 };
 
 export default Home;
