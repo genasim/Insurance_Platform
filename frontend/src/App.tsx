618c3840bf02ddf6f7087944b47855eda32d7082
Genadi Kolev <genadimkolev@gmail.com>
2024-05-01 22:28:27 +0300

    npm: add bootstrap to project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "fomantic-ui-css/semantic.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import pages from "./pages/index.ts";
 import PageLayout from "./layout/index.tsx";
 import NotFoundPage from "./pages/404.tsx";
