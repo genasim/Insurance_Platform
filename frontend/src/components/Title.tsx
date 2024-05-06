503f744260e409d3706f18186e6d79809e3cd555
Genadi Kolev <genadimkolev@gmail.com>
2024-05-06 23:11:52 +0300

    change: make title prop of Title optional

--- frontend/src/components/Title.tsx
+++ frontend/src/components/Title.tsx
@@ -1,14 +1,14 @@
 import { FC } from "react";
 
 interface TitleProps {
-  title: string;
+  title?: string;
   meta?: string;
 }
 
 const Title: FC<TitleProps> = ({ title, meta }) => {
   return (
     <div className="mt-3 mb-5 text-center">
-      <h1 className="title mt-5 mb-2">{title.toUpperCase()}</h1>
+      <h1 className="title mt-5 mb-2">{title?.toUpperCase()}</h1>
       <p className="fs-1">{meta}</p>
     </div>
   );
