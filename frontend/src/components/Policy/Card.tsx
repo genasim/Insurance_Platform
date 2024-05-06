15c4fb106c69ca029e13c2e1d13a62ca8afc85e6
Genadi Kolev <genadimkolev@gmail.com>
2024-05-06 21:31:53 +0300

    chore: migrate Policy cards and group their components

---
+++ frontend/src/components/Policy/Card.tsx
@@ -0,0 +1,47 @@
+import { FC } from "react";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { Policy } from "../../models/policy";
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
+
+export interface PolicyCardProps {
+  policy: Policy;
+  onButtonClick?: () => void;
+}
+
+const PolicyCard: FC<PolicyCardProps> = ({ policy, onButtonClick }) => {
+  return (
+    <Card className="rounded-5 shadow border">
+      <Container>
+        <Row>
+          <Col>
+            <Card.Img src={policy.imgSrc} className="p-4" />
+          </Col>
+          <Col className="col d-flex flex-column">
+            <Card.Title className="fs-3 mb-0 mt-3">
+              {policy.header}
+            </Card.Title>
+            <Card.Text className="my-1">{policy.meta}</Card.Text>
+            <Card.Body className="d-flex flex-column justify-content-between">
+              <div className="">
+                {policy.desctiption.map((descr) => (
+                  <Card.Text key={descr} className="fs-5 my-1">
+                    <IoMdCheckmarkCircleOutline /> {descr}
+                  </Card.Text>
+                ))}
+              </div>
+              <Button
+                variant="primary"
+                onClick={onButtonClick}
+                className="w-100 rounded-pill"
+              >
+                Buy online insurance
+              </Button>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Container>
+    </Card>
+  );
+};
+
+export default PolicyCard;
--- frontend/src/models/policy.ts
+++ frontend/src/models/policy.ts
@@ -3,6 +3,7 @@ type PolicyCardId = string;
 export interface PolicyDTO {
   imgSrc: string;
   header: string;
+  meta: string;
   desctiption: string[];
 }
 
@@ -15,18 +16,21 @@ export const mock_policies = [
     id: "1",
     desctiption: ["Buy policy", "Fast delivery"],
     header: "Car insurance",
+    meta: "Best one out there",
     imgSrc: "/car-insurance.svg",
   },
   {
     id: "2",
     desctiption: ["Buy policy", "Fast delivery"],
+    meta: "Best one out there",
     header: "Car insurance",
     imgSrc: "/medical-insurance.svg",
   },
   {
     id: "3",
     desctiption: ["Buy policy", "Fast delivery"],
+    meta: "Best one out there",
     header: "Car insurance",
     imgSrc: "/medical-insurance.svg",
   },
-] as readonly Policy[];
+] as  Policy[];
