5f09464b23a3486ec9824d848f69c06c2e28d325
X-7r3em <45735901+X-7r3em@users.noreply.github.com>
2024-05-07 15:44:32 +0300

    Added navbar

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -1,12 +1,14 @@
-import { FC } from "react";
-import { Outlet } from "react-router-dom";
+import {FC} from "react";
+import {Outlet} from "react-router-dom";
+import Navbar from "./Navbar";
 
 const PageLayout: FC = () => {
-  return (
-    <>
-      <Outlet />
-    </>
-  );
+    return (
+        <>
+            <Navbar/>
+            <Outlet/>
+        </>
+    );
 };
 
 export default PageLayout;
