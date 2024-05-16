1f4bb87bbbd58f97d1480663cc039767aa0479ad
Genadi Kolev <genadimkolev@gmail.com>
2024-05-16 19:23:46 +0300

    feat: post claim form submit data to backend

--- frontend/src/features/claims/SubmitClaim.tsx
+++ frontend/src/features/claims/SubmitClaim.tsx
@@ -1,17 +1,37 @@
+import randomstring from "randomstring";
 import { FC } from "react";
 import { Button, Container } from "react-bootstrap";
 import { useLoaderData, useNavigate } from "react-router-dom";
 import { ClaimDTO } from "../../models/Claim";
-import { ClaimDocumentDTO } from "../../models/ClaimDocument";
+import { ClaimDocument, ClaimDocumentDTO } from "../../models/ClaimDocument";
 import { Policy } from "../../models/Policy";
+import API, { Tables } from "../../shared/api-client/ApiClient";
 import SubmitForm from "./SubmitForm";
 
 const SubmitClaim: FC = () => {
   const policy = useLoaderData() as Policy;
   const navigate = useNavigate();
 
-  const handleOnSubmit = async (claim: ClaimDTO, docs: ClaimDocumentDTO[]) => {
-    console.log(claim, docs);
+  const handleOnSubmit = async (
+    claimData: ClaimDTO,
+    docsData: ClaimDocumentDTO[]
+  ) => {
+    const claimNumber: string = randomstring.generate({
+      length: 8,
+      charset: ["numeric"],
+    });
+    const claim = await API.create(Tables.CLAIMS, {
+      ...claimData,
+      claimNumber,
+    });
+    docsData.forEach(
+      async (doc) =>
+        await API.create<ClaimDocument>(Tables.CLAIM_DOCUMENTS, {
+          ...doc,
+          claimId: claim.id,
+          claimNumber,
+        })
+    );
   };
 
   return (
