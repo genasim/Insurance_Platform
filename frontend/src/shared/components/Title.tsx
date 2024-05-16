e96bb65668608889390c2b6659096a4d6b3fa22e
Genadi Kolev <genadimkolev@gmail.com>
2024-05-16 19:45:14 +0300

    change: reduce font size of Title meta

--- frontend/src/shared/components/Title.tsx
+++ frontend/src/shared/components/Title.tsx
@@ -9,7 +9,7 @@ const Title: FC<TitleProps> = ({ title, meta }) => {
   return (
     <div className="mt-3 mb-5 text-center">
       <h1 className="title mt-5 mb-2">{title?.toUpperCase()}</h1>
-      <p className="fs-1">{meta}</p>
+      <p className="fs-4">{meta}</p>
     </div>
   );
 };
