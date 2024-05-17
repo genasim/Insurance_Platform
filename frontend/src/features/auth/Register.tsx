b5eacd474aa3833257566ec99fe0172fe88db68b
X-7r3em <45735901+X-7r3em@users.noreply.github.com>
2024-05-17 21:24:24 +0300

    extracted user validation function

--- frontend/src/features/auth/Register.tsx
+++ frontend/src/features/auth/Register.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, {ChangeEvent, FormEvent, useCallback, useEffect, useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
 import API, {Tables} from "../../shared/api-client/ApiClient";
@@ -5,6 +6,7 @@ import {Link, useNavigate} from "react-router-dom";
 import {UserDto} from "./UserDto";
 import {Right} from "../../models/Rights";
 import {User} from "../../models/User";
+import {validateUser} from "../../shared/user-validation/UserValidationUtil";
 
 interface RegisterState {
     email: string | undefined,
@@ -87,63 +89,8 @@ const Register: React.FC = () => {
     }
 
     const validateRegisterData = useCallback(() => {
-        let isValid = true;
-        const emailErrors: string[] = [];
-        if (!state.email || !state.email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)) {
-            emailErrors.push("Invalid email address");
-            isValid = false;
-        }
-
-        const passwordErrors: string[] = [];
-
-        if (state.password !== state.passwordConfirm) {
-            passwordErrors.push("Passwords do not match");
-            isValid = false;
-        }
-
-        if (!state.password || state.password.length < 8) {
-            passwordErrors.push("Password must be at least 8 characters");
-            isValid = false;
-        }
-
-        if (!!state.password && !state.password.match(/\d+/g)) {
-            passwordErrors.push("Password must contain a number");
-            isValid = false;
-        }
-
-        if (!!state.password && state.password === state.password.toUpperCase()) {
-            passwordErrors.push("The password must contain a lower case character");
-            isValid = false;
-        }
-
-        if (!!state.password && state.password === state.password.toLowerCase()) {
-            passwordErrors.push("The password must contain an upper case character");
-            isValid = false;
-        }
-
-        if (!!state.password && !state.password.match(/[!@#$%^&]/g)) {
-            passwordErrors.push("Password must contain !, @, #, $, %, ^ or &");
-            isValid = false;
-        }
-
-        const fullNameErrors: string[] = [];
-        if (!state.fullName) {
-            fullNameErrors.push("Full name must not be empty");
-            isValid = false;
-        }
-
-        const idNumberErrors: string[] = [];
-        if (!state.idNumber || state.idNumber.length !== 10) {
-            idNumberErrors.push("Id number must be 10 symbols");
-            isValid = false;
-        }
-
-        if (!!state.idNumber && !state.idNumber.match(/\d{10}/g)) {
-            idNumberErrors.push("Id number must be only digits");
-            isValid = false;
-        }
-
-        return {isValid, emailErrors, passwordErrors, fullNameErrors, idNumberErrors};
+        const validationResult  = validateUser(state);
+        return validationResult;
     }, [state.email, state.fullName, state.idNumber, state.password, state.passwordConfirm])
 
 
