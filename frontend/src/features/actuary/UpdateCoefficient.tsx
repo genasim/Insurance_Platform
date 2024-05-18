e892220a1bde8e18beb739b7b9f650c9c0df97ad
X-7r3em <45735901+X-7r3em@users.noreply.github.com>
2024-05-18 19:57:43 +0300

    Added coefficient numbers

--- frontend/src/features/actuary/UpdateCoefficient.tsx
+++ frontend/src/features/actuary/UpdateCoefficient.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, {ChangeEvent, FormEvent, useEffect, useState} from 'react';
 import {useNavigate, useParams} from "react-router-dom";
-import {Right} from "../../models/Rights";
 import API, {Tables} from "../../shared/api-client/ApiClient";
 import {IdType} from "../../models/Identifiable";
 import {PolicyType} from "../../models/PolicyType";
@@ -14,6 +13,8 @@ interface CoefficientUpdateState {
     description: string,
     values: CalculationCoefficientValue[],
     isEnabled: boolean,
+    coefficientName: string,
+    coefficientValue: number,
     error: string,
     message: string,
 }
@@ -27,6 +28,8 @@ const UpdateCoefficient: React.FC = () => {
         description: '',
         values: [],
         isEnabled: true,
+        coefficientName: '',
+        coefficientValue: 0,
         error: '',
         message: ''
     });
@@ -90,6 +93,19 @@ const UpdateCoefficient: React.FC = () => {
         });
     }
 
+    const handleAddCoefficientValue = (event: React.MouseEvent<HTMLDivElement>) => {
+        if(!state.coefficientName || !state.coefficientValue) {
+            return;
+        }
+
+        const updatedValues = Array.from(state.values);
+        updatedValues.push({name: state.coefficientName, value: state.coefficientValue})
+        setState({
+            ...state,
+            values: updatedValues,
+        });
+    }
+
     useEffect(() => {
         API.findById<CalculationCoefficient>(Tables.CALCULATION_COEFFICIENTS, coefficientId as IdType)
             .then(coefficient => {
@@ -142,34 +158,23 @@ const UpdateCoefficient: React.FC = () => {
     // }, [state.email, state.fullName]);
 
     const handleOnChange = (event: ChangeEvent<HTMLInputElement>): void => {
-        // if (event.target.name !== "right") {
-        //     setState(prevState => ({
-        //         ...prevState,
-        //         isEdited: true,
-        //         message: '',
-        //         [event.target.name]: event.target.value
-        //     }));
-        //     return;
-        // }
-        // const isValidRight = Object.keys(Right).some(r => r === event.target.value);
-        // if (!isValidRight) {
-        //     throw new Error("Invalid right type in checkbox")
-        // }
-        //
-        // const rights: Set<Right> = new Set<Right>(state.rights);
-        // if (event.target.checked) {
-        //     rights.add(event.target.value as Right);
-        // } else {
-        //     rights.delete(event.target.value as Right);
-        // }
-        // setState(prevState => ({
-        //     ...prevState,
-        //     isEdited: true,
-        //     message: '',
-        //     rights
-        // }));
-    };
+        let value: any = event.target.value;
+        if (event.target.name === "isEnabled") {
+            value = value === "true";
+        }
 
+        if (event.target.name === "coefficientValue") {
+            value = parseFloat(value);
+        }
+
+        setState(prevState => ({
+            ...prevState,
+            isEdited: true,
+            message: '',
+            [event.target.name]: value
+        }));
+    };
+    //ToDo why some form post to the url react
     return (
         <div className="container my-5">
             <button className="btn btn-secondary d-inline me-4 mb-4" onClick={() => navigate(-1)}>Back</button>
@@ -219,14 +224,45 @@ const UpdateCoefficient: React.FC = () => {
                         </div>
                     ))}
                 </div>
+                <div className="col-md-5 mb-4 d-flex justify-content-center">
+                    <input className="form-check-input me-2" type="checkbox"
+                           id="is-enabled"
+                           name="isEnabled"
+                           value={state.isEnabled.toString()}
+                           onChange={handleOnChange}
+                    />
+                    <label className="form-check-label me-4" htmlFor="is-enabled">Is enabled</label>
+                </div>
+
                 {/*{state.error &&*/}
                 {/*    <div className="mb-4 text-danger text-center">{state.error}</div>}*/}
                 {/*{state.message &&*/}
                 {/*    <div className="mb-4 text-success text-center">{state.message}</div>}*/}
-                <div className="mb-4 text-center">
-                    <button type="submit" className="btn btn-primary">Update user</button>
+                <div className="text-center">
+                    <button type="submit" className="btn btn-primary">Update coefficient</button>
                 </div>
             </form>
+            <div className="col-md-3">
+                <label htmlFor="coefficient-name" className="form-label">Coefficient name:</label>
+                <input type="text" className="form-control mb-2" id="coefficient-name"
+                       name="coefficientName"
+                       value={state.coefficientName}
+                       onChange={handleOnChange}
+                       placeholder="name"/>
+            </div>
+            <div className="col-md-3">
+                <label htmlFor="coefficient-value" className="form-label">Coefficient value:</label>
+                <input type="number" className="form-control mb-4" id="coefficient-value"
+                       name="coefficientValue"
+                       value={state.coefficientValue}
+                       onChange={handleOnChange}
+                       placeholder="value"/>
+            </div>
+            <div className="col-md-3 justify-content-center text-center">
+                <div className="btn btn-primary" onClick={(event) => handleAddCoefficientValue(event)}>
+                    Add Coefficient value
+                </div>
+            </div>
         </div>
     );
 };
