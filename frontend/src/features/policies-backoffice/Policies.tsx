7c18e779b78321c56caa71fde3f0e235b5c5cf94
X-7r3em <45735901+X-7r3em@users.noreply.github.com>
2024-05-19 16:52:55 +0300

    Finished register

--- frontend/src/features/policies-backoffice/Policies.tsx
+++ frontend/src/features/policies-backoffice/Policies.tsx
@@ -1,9 +1,225 @@
-import React from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
+import {Policy} from "../../models/Policy";
+import {useNavigate} from "react-router-dom";
+import API, {Tables} from "../../shared/api-client/ApiClient";
+import {IdType} from "../../models/Identifiable";
+import {User} from "../../models/User";
+import {PolicyPackages} from "../../models/PolicyPackages";
+
+interface PoliciesState {
+    policies: PolicyDto[];
+    pageCount: number;
+    currentPage: number;
+    pageSize: number;
+    numberFilter: string;
+    holderFilter: string;
+}
+
+interface PolicyDto {
+    id: IdType;
+    policyNumber: string,
+    holderId: string,
+    holderName: string,
+    type: string,
+    packageId: string,
+    package: string,
+    beginDate: Date,
+    endDate: Date,
+    purchaseDate: Date,
+}
 
 const Policies: React.FC = () => {
+    const [state, setState] = useState<PoliciesState>(
+        {
+            policies: [],
+            pageCount: 1,
+            currentPage: 1,
+            pageSize: 5,
+            numberFilter: '',
+            holderFilter: ''
+        }
+    );
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        API.findAll<Policy>(Tables.POLICIES)
+            .then(policies => {
+                return API.findAll<User>(Tables.USERS).then(users => ({
+                    users,
+                    policies
+                }))
+            })
+            .then(dto => {
+                debugger;
+                return API.findAll<PolicyPackages>(Tables.POLICY_PACKAGES)
+                    .then(packages => ({
+                    ...dto,
+                    packages
+                }))
+            })
+            .then(dto => {
+                const policyDTOs = dto.policies.map(p => {
+                    console.log(p)
+                    const user = dto.users.find(u => u.id === p.holderId)!;
+                    debugger;
+                    const policyPackage = dto.packages.find(pack =>  pack.id === p.packageId)!;
+                    if (!policyPackage) {
+                        console.log(p.packageId)
+                    }
+                    const policyDTO: PolicyDto = {
+                        ...p,
+                        holderName: user.fullName,
+                        package: policyPackage.name
+                    }
+                    return policyDTO;
+                });
+                const filteredPolicies = filterPolicies(policyDTOs);
+                const pageCount = calculatePageCount(filteredPolicies);
+                setState({
+                    ...state,
+                    policies: filteredPolicies,
+                    pageCount: pageCount,
+                    currentPage: state.currentPage <= pageCount ? state.currentPage : 1,
+                });
+            })
+    }, [state.currentPage, state.numberFilter, state.holderFilter]);
+
+    const handleOnPreviousPageClick = () => {
+        if (state.currentPage <= 1) {
+            return;
+        }
+
+        setState({
+            ...state,
+            currentPage: state.currentPage - 1,
+        })
+    };
+
+    const handleSelectedPageClick = (pageNumber: number) => {
+        if (pageNumber < 1 || state.pageCount < pageNumber) {
+            return;
+        }
+
+        setState({
+            ...state,
+            currentPage: pageNumber,
+        })
+    };
+
+    const handleOnNextPageClick = () => {
+        if (state.currentPage >= state.pageCount) {
+            return;
+        }
+
+        setState({
+            ...state,
+            currentPage: state.currentPage + 1,
+        })
+    };
+
+    const calculatePageCount = (policies: Policy[]) => {
+        const remainingUsers = policies.length % state.pageSize;
+        const remainingPage: number = remainingUsers > 0 ? 1 : 0;
+        const pageCount: number = Math.trunc(policies.length / state.pageSize + remainingPage);
+        return pageCount;
+    };
+
+    const getBeginIndex = (): number => {
+        return (state.currentPage - 1) * state.pageSize;
+    }
+
+    const getEndIndex = (): number => {
+        return state.currentPage * state.pageSize;
+    }
+
+    const filterPolicies = (policies: PolicyDto[]) => {
+        if (!!state.numberFilter) {
+            policies = policies.filter((p: PolicyDto) => p.policyNumber.includes(state.numberFilter));
+        }
+
+        if (!!state.holderFilter) {
+            policies = policies.filter((p: PolicyDto) => p.holderName.includes(state.holderFilter));
+        }
+
+        return policies;
+    }
+
+    const handleOnChange = (event: ChangeEvent<HTMLInputElement | HTMLSelectElement>): void => {
+        setState(prevState => ({
+            ...prevState,
+            [event.target.name]: event.target.value
+        }));
+    }
+
     return (
-        <div>
-            !!
+        <div className="container-md">
+            <h2>Manage users</h2>
+            <div className="mb-4 input-group" style={{width: "30%", minWidth: "fit-content"}}>
+                <span className="input-group-text">Filter by number:</span>
+                <input type="text" className="form-control" id="id-number-filter"
+                       name="numberFilter"
+                       onChange={handleOnChange}
+                       placeholder="8804127324"/>
+            </div>
+            <div className="mb-4 input-group" style={{width: "30%", minWidth: "fit-content"}}>
+                <span className="input-group-text">Filter by holder name:</span>
+                <input type="text" className="form-control" id="email-filter"
+                       name="holderFilter"
+                       onChange={handleOnChange}
+                       placeholder="george@domains.com"/>
+            </div>
+            <table className="table">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Number</th>
+                    <th scope="col">Holder</th>
+                    <th scope="col">Package</th>
+                    <th scope="col">Begin date</th>
+                    <th scope="col">End date</th>
+                    <th scope="col">Purchase date</th>
+                    <th scope="col" className="text-end">
+                        <span className="me-4">Actions</span>
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                {state.policies
+                    .filter((_, index) => getBeginIndex() <= index && index < getEndIndex())
+                    .map((policy, index) => (
+                        <React.Fragment key={policy.id}>
+                            <tr>
+                                <th scope="row">{(state.currentPage - 1) * state.pageSize + index + 1}</th>
+                                <td>{policy.policyNumber}</td>
+                                <td>{policy.holderName}</td>
+                                <td>{policy.package}</td>
+                                <td>{policy.beginDate.toString()}</td>
+                                <td>{policy.endDate.toString()}</td>
+                                <td>{policy.purchaseDate.toString()}</td>
+                                <td className="text-end">
+                                    <button className="btn btn-primary me-3" onClick={() => { navigate(`users/${policy.id}`)}}>
+                                        Details</button>
+                                </td>
+                            </tr>
+                        </React.Fragment>
+                    ))}
+                </tbody>
+            </table>
+            <nav aria-label="Manage users pagination" className="navbar justify-content-end">
+                <ul className="pagination">
+                    <li className="page-item" key={0}><a className="page-link"
+                                                         onClick={handleOnPreviousPageClick}>Previous</a></li>
+                    {Array.from({length: state.pageCount}, (_, i) => i + 1).map(number =>
+                        (<li key={number} className="page-item" onClick={() => handleSelectedPageClick(number)}>
+                            <a className="page-link">{number}</a>
+                        </li>))
+                    }
+                    <li className="page-item" key={state.pageCount + 1}><a className="page-link"
+                                                                           onClick={handleOnNextPageClick}>Next</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     );
 };
