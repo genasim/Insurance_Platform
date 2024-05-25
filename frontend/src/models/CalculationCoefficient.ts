3673171795ebf26e5266ede91fbc9d455a272673
X-7r3em <45735901+X-7r3em@users.noreply.github.com>
2024-05-25 17:25:02 +0300

    Added comment

--- frontend/src/models/CalculationCoefficient.ts
+++ frontend/src/models/CalculationCoefficient.ts
@@ -2,6 +2,7 @@ import {Identifiable} from "./Identifiable";
 import {PolicyType} from "./PolicyType";
 import {CalculationCoefficientValue} from "./CalculationCoefficientValue";
 
+//ToDo maybe use classes for models
 export interface CalculationCoefficient extends Identifiable {
     policyType: PolicyType,
     type: string,
