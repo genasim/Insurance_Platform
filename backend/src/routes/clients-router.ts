323d7952d0f0b7c5762120f0bc5af3eab9f80c2d
Genadi Kolev <genadimkolev@gmail.com>
2024-06-21 01:32:08 +0300

    move post policies to client router

--- backend/src/routes/clients-router.ts
+++ backend/src/routes/clients-router.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import createClaimHandler from "../controlers/claims/create-claim";
 import getClaimsPaginatedHandler from "../controlers/claims/get-claims-paginated";
+import createPolicyHandler from "../controlers/policies/create-policy";
 
 const clientsRouter = Router();
 
 clientsRouter.get("/claims", getClaimsPaginatedHandler)
 clientsRouter.post("/claims", createClaimHandler);
+
+clientsRouter.post("/policies", createPolicyHandler)
+
 
 export default clientsRouter;
--- backend/src/routes/expert-router.ts
+++ backend/src/routes/expert-router.ts
@@ -2,14 +2,11 @@ import { Router } from "express";
 import createPolicyPackageHandler from "../controlers/policy-packages/create-policy-package";
 import deletePolicyPackageHandler from "../controlers/policy-packages/delete-policy-package";
 import getPolicyPackageHandler from "../controlers/policy-packages/get-policy-package-id";
-import createPolicyHandler from "../controlers/policies/create-policy";
 
 const epxertRouter = Router()
 
 epxertRouter.get("/policy-packages/:id", getPolicyPackageHandler)
 epxertRouter.post("/policy-packages", createPolicyPackageHandler)
 epxertRouter.delete("/policy-packages/:id", deletePolicyPackageHandler)
-
-epxertRouter.post("/policies", createPolicyHandler)
 
 export default epxertRouter;
