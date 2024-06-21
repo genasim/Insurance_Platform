95efea3e04cf788e2ae6eaa0b2d6df7f70de6a90
Genadi Kolev <genadimkolev@gmail.com>
2024-06-21 23:35:15 +0300

    fix: navbar doesn't change to logout upon logging in

--- frontend/src/shared/layout/Layout.tsx
+++ frontend/src/shared/layout/Layout.tsx
@@ -1,31 +1,34 @@
-import React, {useState} from "react";
-import {Outlet} from "react-router-dom";
+import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import LoggedInContext from "../hooks/useLoggedIn";
+import { AuthStorageKeys } from "../enums/AuthStorageKeys";
 
 interface LayoutState {
-    isLoggedIn: boolean;
+  isLoggedIn: boolean;
 }
 
 const Layout: React.FC = () => {
-    const [state, setState] = useState<LayoutState>({
-        isLoggedIn: false
-    });
-    const setLoggedIn = (isLoggedIn: boolean) => {
-        setState(({
-            ...state,
-            isLoggedIn: isLoggedIn
-        }));
-    };
-    return (
-        <div className="d-flex flex-column min-vh-100">
-            <Navbar setLoggedIn={setLoggedIn}/>
-            <div className="flex-grow-1">
-                <Outlet/>
-            </div>
-            <Footer/>
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(
+    !!sessionStorage.getItem(AuthStorageKeys.TOKEN)
+  );
+  const handleSetLoggedIn = (value: boolean) => {
+    setIsLoggedIn(value);
+  };
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <LoggedInContext.Provider
+        value={{ loggedIn: isLoggedIn, setLoggedIn: handleSetLoggedIn }}
+      >
+        <Navbar />
+        <div className="flex-grow-1">
+          <Outlet />
         </div>
-    );
+        <Footer />
+      </LoggedInContext.Provider>
+    </div>
+  );
 };
 
 export default Layout;
--- frontend/src/shared/layout/Navbar.tsx
+++ frontend/src/shared/layout/Navbar.tsx
@@ -1,106 +1,109 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import {Link} from "react-router-dom";
+import { useContext, useRef } from "react";
+import { Link } from "react-router-dom";
+import { Right } from "../../models/Rights";
 import Logo from "../components/Logo";
-import {AuthStorageKeys} from "../enums/AuthStorageKeys";
-import {Right} from "../../models/Rights";
+import { AuthStorageKeys } from "../enums/AuthStorageKeys";
+import useJwt from "../hooks/useJwt";
+import LoggedInContext from "../hooks/useLoggedIn";
 
+const Navbar = () => {
+  const jwt = useJwt();
+  const rights = useRef(jwt.rights);
+  const { loggedIn, setLoggedIn } = useContext(LoggedInContext);
 
-interface NavbarProps {
-    setLoggedIn: (isLoggedIn: boolean) => void
-}
+  const handleLogout = () => {
+    sessionStorage.removeItem(AuthStorageKeys.TOKEN);
+    setLoggedIn(false);
+  };
 
-const Navbar = ({setLoggedIn}: NavbarProps) => {
-    const isLoggedIn = !!sessionStorage.getItem(AuthStorageKeys.TOKEN);
-    const rights = sessionStorage.getItem(AuthStorageKeys.RIGHTS)?.split(",") ?? [];
-
-    let login = null;
-    let register = null;
-    let logout = null;
-    let admin = null;
-
-    const handleLogout = () => {
-        sessionStorage.removeItem(AuthStorageKeys.TOKEN);
-        sessionStorage.removeItem(AuthStorageKeys.RIGHTS);
-        sessionStorage.removeItem(AuthStorageKeys.USER_ID);
-        setLoggedIn(false);
-    };
-
-    if (!isLoggedIn) {
-        login =
+  return (
+    <nav
+      style={{ borderRadius: "0 0 8em 8em" }}
+      className="navbar navbar-expand-md bg-primary-subtle py-4 px-5"
+    >
+      <div className="container-xxl">
+        <div className="d-none d-sm-block">
+          <Logo />
+        </div>
+        <button
+          className="navbar-toggler float-end"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#main-nav"
+          aria-controls="main-nav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          id="main-nav"
+          className="collapse navbar-collapse justify-content-end align-center"
+        >
+          <ul className="navbar-nav">
             <li className="nav-item fs-5">
-                <Link className="nav-link" to="/login">Login</Link>
-            </li>;
-        register =
-            <li className="nav-item fs-5">
-                <Link className="nav-link" to="/register">Register</Link>
-            </li>;
-    } else {
-        logout =
-            <li className="nav-item fs-5">
-                <Link className="nav-link" to="/" onClick={handleLogout}>Logout</Link>
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
             </li>
-    }
-
-    if (rights.includes(Right.ADMIN)) {
-        admin =
             <li className="nav-item fs-5">
-                <Link className="nav-link" to="/admin">Admin</Link>
-            </li>;
-    }
-
-    return (
-        <nav
-            style={{borderRadius: "0 0 8em 8em"}}
-            className="navbar navbar-expand-md bg-primary-subtle py-4 px-5">
-            <div className="container-xxl">
-                <div className="d-none d-sm-block">
-                    <Logo/>
-                </div>
-                <button
-                    className="navbar-toggler float-end"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#main-nav"
-                    aria-controls="main-nav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div id="main-nav" className="collapse navbar-collapse justify-content-end align-center">
-                    <ul className="navbar-nav">
-                        <li className="nav-item fs-5">
-                            <Link className="nav-link" to="/">Home</Link>
-                        </li>
-                        <li className="nav-item fs-5">
-                            <Link to="/client/policies" className="nav-link">
-                                Policies
-                            </Link>
-                        </li>
-                        <li className="nav-item fs-5">
-                            <Link to="/client/claims" className="nav-link">
-                                Claims
-                            </Link>
-                        </li>
-                        <li className="nav-item fs-5">
-                            <Link className="nav-link" to="/backoffice/claims">Backoffice</Link>
-                        </li>
-                        <li className="nav-item fs-5">
-                            <Link className="nav-link" to="/backoffice/policies">Policies BO</Link>
-                        </li>
-                        <li className="nav-item fs-5">
-                            <Link className="nav-link" to="/actuary">Actuary</Link>
-                        </li>
-                        {login}
-                        {register}
-                        {admin}
-                        {logout}
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
+              <Link to="/client/policies" className="nav-link">
+                Policies
+              </Link>
+            </li>
+            <li className="nav-item fs-5">
+              <Link to="/client/claims" className="nav-link">
+                Claims
+              </Link>
+            </li>
+            <li className="nav-item fs-5">
+              <Link className="nav-link" to="/backoffice/claims">
+                Backoffice
+              </Link>
+            </li>
+            <li className="nav-item fs-5">
+              <Link className="nav-link" to="/backoffice/policies">
+                Policies BO
+              </Link>
+            </li>
+            <li className="nav-item fs-5">
+              <Link className="nav-link" to="/actuary">
+                Actuary
+              </Link>
+            </li>
+            {rights.current.includes(Right.ADMIN) && (
+              <li className="nav-item fs-5">
+                <Link className="nav-link" to="/admin">
+                  Admin
+                </Link>
+              </li>
+            )}
+            {!loggedIn && (
+              <>
+                <li className="nav-item fs-5">
+                  <Link className="nav-link" to="/login">
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item fs-5">
+                  <Link className="nav-link" to="/register">
+                    Register
+                  </Link>
+                </li>
+              </>
+            )}
+            {loggedIn && (
+              <li className="nav-item fs-5">
+                <Link className="nav-link" to="/" onClick={handleLogout}>
+                  Logout
+                </Link>
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
