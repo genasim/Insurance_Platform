f3690f3118fd780467f3bd2b28d6a641d4510fbe
Genadi Kolev <genadimkolev@gmail.com>
2024-06-23 00:35:59 +0300

    feat: add check-valid-email controller

--- backend/src/routes/auth-router.ts
+++ backend/src/routes/auth-router.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import registerClientHandler from "../controlers/register-user";
 import loginUserHandler from "../controlers/login-user";
+import checkValidEmail from "../controlers/check-valid-email";
 
 const authRouter = Router()
 
 authRouter.post("/register", registerClientHandler)
 authRouter.post("/login", loginUserHandler)
+authRouter.post("/valid-email",checkValidEmail)
 
 export default authRouter
