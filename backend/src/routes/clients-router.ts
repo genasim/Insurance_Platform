31b269843b524197aba39a9e21e377615da1bfa9
Genadi Kolev <genadimkolev@gmail.com>
2024-06-23 00:36:24 +0300

    feat: add get-policy-type-packages controller

--- backend/src/routes/clients-router.ts
+++ backend/src/routes/clients-router.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import createClaimHandler from "../controlers/claims/create-claim";
 import getClaimsPaginatedHandler from "../controlers/claims/get-claims-paginated";
 import createPolicyHandler from "../controlers/policies/create-policy";
+import getPolicyTypePackagesHandler from "../controlers/policy-packages/get-policy-type-packages";
 
 const clientsRouter = Router();
 
@@ -9,6 +10,7 @@ clientsRouter.get("/claims", getClaimsPaginatedHandler)
 clientsRouter.post("/claims", createClaimHandler);
 
 clientsRouter.post("/policies", createPolicyHandler)
+clientsRouter.get("/policies/:type/packages", getPolicyTypePackagesHandler)
 
 
 export default clientsRouter;
