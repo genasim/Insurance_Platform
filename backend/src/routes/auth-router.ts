8aa0c226974a101ca93461087f0aa6bb16615d2e
Genadi Kolev <genadimkolev@gmail.com>
2024-06-23 17:57:36 +0300

    change: add policy-template model and get controller

--- backend/src/routes/auth-router.ts
+++ backend/src/routes/auth-router.ts
@@ -2,11 +2,14 @@ import { Router } from "express";
 import registerClientHandler from "../controlers/register-user";
 import loginUserHandler from "../controlers/login-user";
 import checkValidEmail from "../controlers/check-valid-email";
+import getPolicyTemplatesHandler from "../controlers/policies/get-policy-templates";
 
 const authRouter = Router()
 
 authRouter.post("/register", registerClientHandler)
 authRouter.post("/login", loginUserHandler)
 authRouter.post("/valid-email",checkValidEmail)
+authRouter.get("/policy-types", getPolicyTemplatesHandler)
+
 
 export default authRouter
