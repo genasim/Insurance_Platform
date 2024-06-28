03ee8eadf57a210804bf673ce1aded5d0dcb3246
X-7r3em <45735901+X-7r3em@users.noreply.github.com>
2024-06-28 15:26:45 +0300

    Updated update user admin

--- backend/src/routes/admin-router.ts
+++ backend/src/routes/admin-router.ts
@@ -3,12 +3,14 @@ import createUserHandler from "../controlers/users/create-user";
 import deleteUserHandler from "../controlers/users/delete-user";
 import getUsersPaginatedHandler from "../controlers/users/get-users-paginated";
 import getUserHandler from "../controlers/users/get-user";
+import updateUserHandler from "../controlers/users/update-user";
 
 const adminRouter = Router()
 
 adminRouter.get("/users/", getUsersPaginatedHandler)
 adminRouter.get("/users/:id", getUserHandler)
 adminRouter.post("/users/", createUserHandler)
+adminRouter.patch("/users/:id", updateUserHandler)
 adminRouter.delete("/users/:id", deleteUserHandler)
 
 
