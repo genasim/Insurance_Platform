1ec63269d3fae41af9822bbaa5ad0f591e867d64
X-7r3em <45735901+X-7r3em@users.noreply.github.com>
2024-06-28 13:51:19 +0300

    Updated user backend

--- backend/src/controlers/users/get-users-paginated.ts
+++ backend/src/controlers/users/get-users-paginated.ts
@@ -1,32 +1,49 @@
 import { Request, RequestHandler, Response } from "express";
 import usersModel from "../../models/users.model";
 
+type QueryParams = {
+    page: string | number;
+    size: string | number;
+    email: string;
+    number: string;
+};
+
 const getUsersPaginatedHandler: RequestHandler = async (
-  req: Request,
-  res: Response
+    req: Request,
+    res: Response
 ) => {
-  let { page, size } = req.query;
-  page = page === "" || !page ? "1" : page;
-  size = page === "" || !size ? "10" : size;
+    let { page, size, email, number } = req.query as QueryParams;
+    page = page === "" || !page ? "1" : page;
+    size = page === "" || !size ? "10" : size;
 
-  if (isNaN(+page) || isNaN(+size)) {
-    res.status(400).json({ message: "Invalid page or size info" });
-    return;
-  }
+    if (isNaN(+page) || isNaN(+size)) {
+        res.status(400).json({ message: "Invalid page or size info" });
+        return;
+    }
+    page = Number(page);
+    size = Number(size);
 
-  try {
-    const users = await usersModel.aggregate([
-      { $skip: (+page - 1) * +size },
-      { $limit: +size },
-    ]);
-    users.forEach((user) => {
-      delete user.password;
-    });
-    res.status(200).json(users);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server Error" });
-  }
+    const emailRegex = new RegExp(email || ".*", "i");
+    const numberRegex = new RegExp(number || ".*", "i");
+
+    try {
+        const users = await usersModel
+            .find({
+                email: { $regex: emailRegex },
+                idNumber: { $regex: numberRegex },
+            })
+            .skip((page - 1) * size)
+            .limit(size);
+
+        users.forEach((user) => {
+            delete user.password;
+        });
+
+        res.status(200).json(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server Error" });
+    }
 };
 
 export default getUsersPaginatedHandler;
