3f9bcc1600adf4a3899640750b7fc355643186d4
X-7r3em <45735901+X-7r3em@users.noreply.github.com>
2024-06-30 18:39:04 +0300

    Removed unneeded usages

--- backend/src/routes/notifications-router.ts
+++ backend/src/routes/notifications-router.ts
@@ -1,9 +1,4 @@
 import { Router } from "express";
-import getCoefficientsPaginatedHandler from "../controlers/coefficients/get-coefficients-paginated";
-import deleteCoefficientHandler from "../controlers/coefficients/delete-coefficient";
-import createUserHandler from "../controlers/coefficients/create-coefficient";
-import getCoefficientHandler from "../controlers/coefficients/get-coefficient";
-import updateCoefficientHandler from "../controlers/coefficients/update-coefficient";
 import getNotificationsPaginated from "../controlers/notifications/get-notifications-paginated";
 
 const notificationRouter = Router();
